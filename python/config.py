399a098185eb8c0f6bc12d957b2c6cc266d07c6d
Ofer Chen <oferchen@gmail.com>
2024-01-19 23:22:17 +0100

    added reconnect parameter

--- python/config.py
+++ python/config.py
@@ -3,4 +3,5 @@
 class Config:
     ETCD_DEFAULT_HOST = "localhost"
     ETCD_DEFAULT_PORT = 2379
-    ETCD_HOSTS_BASE_DIR = "hosts/"
+    ETCD_ALLOW_RECONNECT = True,
+    ETCD_HOSTS_BASE_DIR = "/hosts/"
